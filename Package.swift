a096872468de9ab99e5808064a7d65565ca29102
Pedro José Pereira Vieito <pvieito@gmail.com>
2020-10-13 17:53:31 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Radar-STATS-Swift",
+    name: "RadarSTATSKit",
     products: [
         .library(
             name: "RadarSTATSKit",
